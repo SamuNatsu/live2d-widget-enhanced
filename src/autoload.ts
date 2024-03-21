87ddfb8e17c695406c79e45253e66ecdcb69880c
SNRainiar <rainiar@foxmail.com>
2024-03-21 15:52:25 +0800

    Update

--- src/autoload.ts
+++ src/autoload.ts
@@ -4,5 +4,5 @@ import { RemoteCdn, init } from './core';
 // Initialize
 init({
   api: new RemoteCdn('https://fastly.jsdelivr.net/gh/fghrsh/live2d_api'),
-  resource: 'https://fastly.jsdelivr.net/gh/stevenjoezhang/live2d-widget@latest'
+  resource: 'https://fastly.jsdelivr.net/gh/samunatsu/live2d-widget-enhanced@latest/dist'
 });
